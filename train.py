76bed1c656c0673f5e657fe4a390388b5a96a19b
Gabriel Nakamoto <gabriel@nakamoto.ca>
2026-06-12 21:07:07 -0400

    init

---
+++ train.py
@@ -0,0 +1,73 @@
+from tinygrad.dtype import dtypes
+from tinygrad.tensor import Tensor
+from tinygrad.nn.optim import AdamW
+from tinygrad.nn import datasets
+from tinygrad.nn.state import get_parameters
+from tinygrad.engine.jit import TinyJit
+from vit import ViT
+
+batch_size, ishape, log_steps = 256, (32, 32, 3), 50
+X_train, Y_train, X_test, Y_test = datasets.cifar()
+X_train, X_test = X_train.permute(0,2,3,1), X_test.permute(0,2,3,1)
+X_train, X_test = X_train.float() / 255.0, X_test.float() / 255.0
+X_test, Y_test = X_test[:1000], Y_test[:1000] # limited by laptop VRAM
+
+def random_flip(x:Tensor): return (Tensor.rand(x.shape[0],1,1,1) < 0.5).where(x.flip(-1), x).contiguous()
+def random_crop(x:Tensor,crop_size=32):
+  B, h, w, _ = x.shape
+  Xs = Tensor.randint(B, low=0, high=h-crop_size).reshape(B,1,1,1)
+  Ys = Tensor.randint(B, low=0, high=w-crop_size).reshape(B,1,1,1)
+  Xi = Tensor.arange(crop_size, dtype=dtypes.uint32).reshape(1,1,1,crop_size)
+  Yi = Tensor.arange(crop_size, dtype=dtypes.uint32).reshape(1,1,crop_size,1)
+  x_strip = (Xs + Xi).expand(-1, 3, h, -1)
+  y_strip = (Ys + Yi).expand(-1, 3, crop_size, crop_size)
+  return x.gather(1, x_strip).gather(2, y_strip)
+
+# TODO: implement myself, batch-wise augmentation
+# def cutmix(X, Y, alpha:float): pass
+
+@TinyJit
+def augment(X,Y):
+  X = random_flip(X)
+  # X = X.pad((None, None, (4,4),(4,4)))
+  # X = random_crop(X)
+  # todo: cutmix?
+  return X, Y
+
+def random_batch() -> tuple[Tensor, Tensor]:
+  idx = Tensor.randint(batch_size, low=0, high=int(X_train.shape[0]))
+  return augment(X_train[idx], Y_train[idx])
+
+def one_cycle_lr(optim,total_steps,initial_lr,warmup_weight:float=0.3,max_lr:float=1e-3):
+  warmup_steps = int(total_steps * warmup_weight)
+  def fn(step):
+    import math
+    if step < warmup_steps: lr = initial_lr + (step / warmup_steps) * (max_lr - initial_lr)
+    else: lr = initial_lr + 0.5 * (1.0 + math.cos(math.pi * (step - warmup_steps) / (total_steps - warmup_steps))) * (max_lr - initial_lr)
+    optim.lr.assign(Tensor([lr]))
+  return fn
+
+model = ViT(192, 3, 10, 4, 32, 32, 3, 10)
+optim = AdamW(get_parameters(model))
+total_steps = 1000
+lr_scheduler = one_cycle_lr(optim,total_steps, 1e-5)
+
+@TinyJit
+def step(x, y):
+  optim.zero_grad()
+  logits = model(x)
+  loss = logits.sparse_categorical_crossentropy(y).backward()
+  optim.step()
+  return loss
+
+for n in range(total_steps):
+  lr_scheduler(n)
+  Tensor.training = True
+  loss = step(*random_batch())
+  Tensor.training = False
+  if n % log_steps == 0:
+    probs = model(X_test).softmax(axis=-1)
+    acc = (Y_test == probs.argmax(-1)).mean()
+    print(f"step={n}\t\tloss={loss.item():.2f}, valid acc={100*acc.item():.2f}%f")
+  else:
+    print(f"step={n}\t\tloss={loss.item():.2f}")
---
+++ vit.py
@@ -0,0 +1,43 @@
+from tinygrad.tensor import Tensor
+from tinygrad.nn import Linear, RMSNorm, Embedding
+
+class TransformerBlock:
+  def __init__(self, dim, n_heads):
+    self.dim = dim
+    self.n_heads = n_heads
+    self.head_dim = dim // n_heads
+    self.qkv_proj = Linear(dim, dim*3)
+    self.attn_norm = RMSNorm(dim)
+    self.mlp = [Linear(dim, dim * 4), Tensor.swish, Linear(dim * 4, dim)]
+    self.mlp_norm = RMSNorm(dim)
+    self.attn_proj = Linear(dim, dim)
+  def _attention(self, x:Tensor, dropout_p:float=0.0) -> Tensor:
+    # (bchsz, N, dim)
+    B, N = x.shape[:2]
+    q, k, v = [t.squeeze(2).transpose(1,2) for t in self.qkv_proj(x).reshape(B,N,3,self.n_heads,self.head_dim).split(1, dim=2)]
+    attn = q.scaled_dot_product_attention(k, v, dropout_p=dropout_p)
+    # shape (B,N,self.head_dim)
+    return self.attn_proj(attn.reshape((B,N,self.dim)))
+  def __call__(self, x:Tensor, dropout_p:float=0.0) -> Tensor:
+      x = x + self._attention(self.attn_norm(x), dropout_p=dropout_p)
+      return x + self.mlp_norm(x).sequential(self.mlp).dropout(dropout_p)
+
+# https://arxiv.org/pdf/2010.11929
+class ViT:
+  def __init__(self, dim, n_heads, layers, num_classes, P, h, w, c):
+    self.dim, self.num_class, self.N = dim, num_classes, (h*w)//(P*P)
+    self.blocks = [TransformerBlock(dim, n_heads) for _ in range(layers)]
+    self.patch_emb = Linear(P*P*c, dim)
+    self.pos_emb = Embedding(self.N + 1, dim)
+    self.out_norm = RMSNorm(dim)
+    self.x_class = Tensor.zeros(1,1, dim)
+    self.head = Linear(dim, num_classes)
+
+  def __call__(self, img:Tensor, dropout_p:float=0.1) -> Tensor:
+    B = img.shape[0]
+    img = img.reshape(B, self.N, -1)
+    _cls = self.x_class.expand(B, 1, self.dim)
+    x = _cls.cat(self.patch_emb(img), dim=1) + self.pos_emb(Tensor.arange(self.N + 1))
+    for t in self.blocks: x = t(x, dropout_p=dropout_p)
+    x = self.out_norm(x)
+    return self.head(x[:, 0])
